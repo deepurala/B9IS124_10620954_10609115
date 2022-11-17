ec2383997939f516b63d7cbeed2a5f45e44f6fb7
Your Name <you@example.com>
2022-11-17 09:35:15 +0000

    Minor changes

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
-<script>
+
           function searchFunction() {
             // Declare variables
             var input, filter, product, i, textValue;
@@ -13,20 +13,53 @@
                         }); 
                       });
           
-            // Loop through all list items, and hide those who don't match the search query
-          //   for (const [key, value] of Object.entries(productDict)){
-          //     product = key.getElementsByTagName(productDict)
-          //   }
+          //Loop through all list items, and hide those who don't match the search query
+             for (const [key, value] of Object.entries(productDict)){
+              product = key.getElementsByTagName(productDict.key)
+             }
 
-          //   for (i = 0; i < productName.length; i++) {
-          //     a = li[i].getElementsByTagName("a")[0];
-          //     textValue = a.textContent || a.innerText;
-          //     if (textValue.toUpperCase().indexOf(filter) > -1) {
-          //       product[i].style.display = ""; 
+             for (i = 0; i < productName.length; i++) {
+               a = filter[i].getElementsByTagName("a")[0];
+               textValue = a.textContent || a.innerText;
+               if (textValue.toUpperCase().indexOf(filter) > -1) {
+                 product[i].style.display = ""; 
           
-          //     } else {
-          //       li[i].style.display = "none";
-          //     }
-          //   }
-          // }
-          </script>
+               } else {
+                 li[i].style.display = "none";
+               }
+             }
+           }
+
+
+
+           function searchProduct(){
+            $(".shop-content").html("");
+            var productname=$("#searchbuttonid").val();
+            if(productname==""){  
+            alert("please enter value for search");  
+            data.forEach( (product)=> {
+                var  productList = '<div class="product-box">'+
+                        '<img src="'+product.img[0]+'" alt="" class="product-img">'+
+                        '<h2 class="product-title">'+product.title+'</h2>'+
+                        '<span class="price">'+product.price+'</span>'+
+                    '<a href="reserve.html"><i class="bx bx-plus reserve" title="Reserve Product"></i></a>'+
+                        '</div>';
+                    $('.shop-content').append(productList);
+            });
+            }
+            else{
+            $(".shop-content").html("");
+            for(var i=0;i<data.length;i++){
+            if(data[i].title.toLowerCase()==productname.toLowerCase()){
+            var  productList = '<div class="product-box">'+
+                        '<img src="'+data[i].img[0]+'" alt="" class="product-img">'+
+                        '<h2 class="product-title">'+data[i].title+'</h2>'+
+                        '<span class="price">'+data[i].price+'</span>'+
+                    '<a href="reserve.html"><i class="bx bx-plus reserve" title="Reserve Product"></i></a>'+
+                        '</div>';
+                    $('.shop-content').append(productList);            
+            }
+            }  
+            }
+            }
+          
